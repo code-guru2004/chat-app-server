d1fe2ae8fa6af679fb9ce57e03f89d4187303e96
Nayan Das <nayan010das@gmail.com>
2025-06-06 21:02:32 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  socket.on('join_room', ({ room, user }) => {
+    socket.join(room);
+    console.log(`${user} joined room ${room}`);
+    socket.to(room).emit('user_joined', { user });
+  });
+
+  socket.on('send_message', ({ room, user, text }) => {
+    io.to(room).emit('receive_message', { user, text });
+  });
+  
+  socket.on('typing', ({ room, user }) => {
+    socket.to(room).emit('user_typing', { user });
+  });
+  
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  }); 
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
